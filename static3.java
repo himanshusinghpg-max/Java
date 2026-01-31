ef0fb5ea0c7895b8e0dc7d5538ceb56669d5acb8
Himanshu Singh <himanshusinghpg@gmail.com>
2026-01-31 09:21:24 +0530

    Updated code and added new programs

---
+++ static3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class static3 {
+
+    static int n;  
+    //int m;          
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+       // static3 M = new static3();
+
+         //int n;              
+        //M.m = s.nextInt();  
+
+        System.out.println(n);
+       // System.out.println(M.m);
+    }
+}
